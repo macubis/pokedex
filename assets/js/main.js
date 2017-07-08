3b8476b36f2153b8f7782a5e804d42570d0c8b07
macubis <macubis@gmail.com>
2017-07-07 22:44:41 -0400

    Subiendo ejercicio Evaluado

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+/*
+<scriptsrc="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js</scr>
+<div id="success">
+<div id="artisturl"></div>
+</div>
+
+$.ajax({
+   type : 'GET',
+   url : 'https://api.spotify.com/v1/search',
+   data : {'q':'coldplay',
+   'type':'artist'
+   },
+   dataType : 'json',
+   success : function(data) {
+      $('#success #artisturl').html(data.artists.items[0].uri) ;
+   },
+}) ;
+*/
